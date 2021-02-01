82b68af29bf2a8731c50c14f33e534b6b4750e21
Shadman Kaif <shadman.kaif@mail.utoronto.ca>
2021-01-31 19:16:24 -0500

    Added new folders for server and deliver

--- lab1/Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo "----------Building project:[ lab1 - Debug ]----------"
-	@cd "lab1" && "$(MAKE)" -f  "lab1.mk"
-clean:
-	@echo "----------Cleaning project:[ lab1 - Debug ]----------"
-	@cd "lab1" && "$(MAKE)" -f  "lab1.mk" clean
---
+++ lab1/deliver/deliver.c
@@ -0,0 +1,19 @@
+// ECE361 
+// Shadman Kaif and Abdurrafay Khan
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main(void)
+{
+	printf("hello world\n");
+	return 0;
+}
