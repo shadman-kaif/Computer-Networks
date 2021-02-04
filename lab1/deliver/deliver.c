94797fe7fd18cd08785fac3b8110de72b393eca4
Shadman Kaif <shadman.kaif@mail.utoronto.ca>
2021-02-04 09:49:48 -0500

    added deliver.c code

--- lab1/deliver/deliver.c
+++ lab1/deliver/deliver.c
@@ -1,5 +1,6 @@
 /*
-** talker.c -- a datagram "client" demo
+** ECE361 - Computer Networks - Lab 1 - Client File - deliver.c
+** Group 21 - Abdurrafay Khan & Shadman Kaif
 */
 
 #include <stdio.h>
@@ -13,55 +14,92 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define SERVERPORT "4950"	// the port users will be connecting to
-
 int main(int argc, char *argv[])
 {
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-
+	// checks for 3 arguments being passed in <deliver> <server address> <port#>
 	if (argc != 3) {
-		fprintf(stderr,"usage: talker hostname message\n");
-		exit(1);
+		fprintf(stderr, "usage: deliver <server address> <server port number>\n");
+		return 0;
+	}
+	
+	// stores arguments passed in to variables
+	char* server_address = argv[1];
+	char* port_num = argv[2];
+	
+	// asks user for file name to transfer
+	printf("Enter file name to transfer:\n");
+	
+	// takes input and stores to given variables
+	char ftp_string[75];
+	char filename[75];
+	scanf("%s %s", ftp_string, filename);
+	
+	// verifying the input for ftp
+    if(strcmp(ftp_string, "ftp")!= 0){
+        printf("Did not start with 'ftp' correctly\n", ftp_string);
+        return 0;
+    }
+	
+	// checks to see if filename exists in directory
+	if (access(filename, F_OK) == -1) {
+		printf("File does not exist\n");
+		return 0;
 	}
 
+	// sets reference information for which socket type to use
+	struct addrinfo hints; 
+	
+	// pointer to linked list of addrinfo's with addresses that match paramenters set by in hints
+	struct addrinfo *res; 
+	
+	// resets hints and updates with desires parameters (IPv4 or IPv6, datagram, udp connection)
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_DGRAM;
-
-	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
+	hints.ai_protocol = IPPROTO_UDP;
+	
+	// fils out res with the given possible addresses to connect to
+	int info = getaddrinfo(server_address, port_num, &hints, &res);
+	
+	// makes a socket connecting to the first address in res and checks for error
+	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sockfd == -1) {
+		printf("Error in making socket\n");
+		return 0;
 	}
-
-	// loop through all the results and make a socket
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("talker: socket");
-			continue;
-		}
-
-		break;
+	
+	// sends a "ftp" message to server and checks if it sent
+	int sent_bytes = sendto(sockfd, "ftp", 3, 0, res->ai_addr, res->ai_addrlen);
+	if (sent_bytes == -1) {
+		printf("Error in sending 'ftp'\n");
+		return 0;
 	}
-
-	if (p == NULL) {
-		fprintf(stderr, "talker: failed to create socket\n");
-		return 2;
+	
+	// address info for the connection being made in order to receieve message from server
+	struct sockaddr_storage connecting_address;
+	socklen_t addr_len = sizeof connecting_address;
+	char received_message[75];
+	
+	// receives message from server and error checks
+	int rec_bytes = recvfrom(sockfd, received_message, 74 , 0, (struct sockaddr *)&connecting_address, &addr_len);
+	if (rec_bytes == -1) {
+		printf("Error in receiving message from server\n");
+		return 0;
 	}
-
-	if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
-			 p->ai_addr, p->ai_addrlen)) == -1) {
-		perror("talker: sendto");
-		exit(1);
+	
+	// verifies "yes" has been receieved from the server
+    if(strcmp(received_message, "yes") == 0){
+        printf("A file transfer can start.\n");
+        return 0;
+    }
+	else {
+		printf("Did not receieve 'yes' from server\n");
+		return 0;
 	}
-
-	freeaddrinfo(servinfo);
-
-	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
-	close(sockfd);
-
+	
+	// fees memory and closes connection
+    freeaddrinfo(res);
+    close(sockfd);
+	
 	return 0;
 }
